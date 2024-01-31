b22c89815c7f85724d6bb19e5af30cfb5f662cd3
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-02-01 00:20:19 +0530

    dalle controller

--- controllers/Dalle.controller.js
+++ controllers/Dalle.controller.js
@@ -1,1 +1,41 @@
-exports.test = () => {};
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+exports.genImage = async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      // response_format: "b64_json",
+    });
+
+    // Check if the aiResponse object and its nested properties exist
+    const image = aiResponse?.data?.[0].url;
+
+    if (image) {
+      res.status(200).json({ url: image });
+    } else {
+      console.error(
+        "Error: Unable to get image data from API response.",
+        aiResponse
+      );
+      res
+        .status(500)
+        .json({ error: "Unable to get image data from API response." });
+    }
+  } catch (error) {
+    console.error(error);
+
+    // Check if the error has a response and data property before accessing nested properties
+    const errorMessage =
+      error?.response?.data?.error?.message || "Something went wrong";
+
+    res.status(error?.response?.status || 500).json({ error: errorMessage });
+  }
+};
--- routes/DalleRoutes.js
+++ routes/DalleRoutes.js
@@ -1,50 +1,9 @@
 const express = require("express");
-const OpenAI = require("openai");
 
 const router = express.Router();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const { DalleController } = require("../controllers");
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL-E!" });
-});
-
-router.route("/").post(async (req, res) => {
-  try {
-    const { prompt } = req.body;
-
-    const aiResponse = await openai.images.generate({
-      prompt,
-      n: 1,
-      size: "1024x1024",
-      // response_format: "b64_json",
-    });
-
-    // Check if the aiResponse object and its nested properties exist
-    const image = aiResponse?.data?.[0].url;
-
-    if (image) {
-      res.status(200).json({ url: image });
-    } else {
-      console.error(
-        "Error: Unable to get image data from API response.",
-        aiResponse
-      );
-      res
-        .status(500)
-        .json({ error: "Unable to get image data from API response." });
-    }
-  } catch (error) {
-    console.error(error);
-
-    // Check if the error has a response and data property before accessing nested properties
-    const errorMessage =
-      error?.response?.data?.error?.message || "Something went wrong";
-
-    res.status(error?.response?.status || 500).json({ error: errorMessage });
-  }
-});
+router.post("/", DalleController.genImage);
 
 module.exports = router;
